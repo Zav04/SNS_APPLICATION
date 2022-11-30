5e639b10a62e473ec364a5d96d95f7693fc16311
BOliveira <bruno.bx04@gmail.com>
2022-11-30 00:57:25 +0000

    VersionNumberUpdate

--- Aplication_SNS/VersionNumber/VersionNumber.Designer.cs
+++ Aplication_SNS/VersionNumber/VersionNumber.Designer.cs
@@ -3,12 +3,12 @@
     partial class VersionNumber
     {
         /// <summary>
-        /// Required designer variable.
+        ///  Required designer variable.
         /// </summary>
         private System.ComponentModel.IContainer components = null;
 
         /// <summary>
-        /// Clean up any resources being used.
+        ///  Clean up any resources being used.
         /// </summary>
         /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         protected override void Dispose(bool disposing)
@@ -23,30 +23,17 @@
         #region Windows Form Designer generated code
 
         /// <summary>
-        /// Required method for Designer support - do not modify
-        /// the contents of this method with the code editor.
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(VersionNumber));
-            this.SuspendLayout();
-            // 
-            // VersionNumber
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.components = new System.ComponentModel.Container();
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.White;
-            this.ClientSize = new System.Drawing.Size(506, 278);
-            this.ControlBox = false;
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
-            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.Name = "VersionNumber";
-            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.ResumeLayout(false);
-
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Text = "Form1";
         }
 
         #endregion
     }
 }
-
--- Aplication_SNS/VersionNumber/VersionNumber_Main.cs
+++ Aplication_SNS/VersionNumber/VersionNumber_Main.cs
@@ -1,34 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Reflection;
-using System.Threading.Tasks;
-using System.Windows.Forms;
-
 namespace VersionNumber
 {
     internal static class VersionNumber_Main
     {
         /// <summary>
-        /// The main entry point for the application.
+        ///  The main entry point for the application.
         /// </summary>
         [STAThread]
         static void Main()
         {
-
-            //GET VERSION
-
-            //var version = Assembly.GetExecutingAssembly().GetName().Version;
-
-            //var buildDate = new DateTime(2000, 1, 1)
-            //   .AddDays(version.Build).AddSeconds(version.Revision * 2);
-
-            //var displayableVersion = $"{version} ({buildDate})";
-
-            //Console.WriteLine("Current version (inc. build date) = " + displayableVersion);
-
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
+            ApplicationConfiguration.Initialize();
             Application.Run(new VersionNumber());
         }
     }
